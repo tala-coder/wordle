39fd581add7bb9c559c4805e8a8bf6947809f468
Tala-coder <muhammedtalic.it@gmail.com>
2022-08-19 17:05:31 +0200

    asd

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState } from 'react';
 import { randomRijec } from '../utils/Rijeci';
 const DataContext = createContext({});
 
